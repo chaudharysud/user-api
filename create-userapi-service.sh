e2457f53f38e71fbc55376988317752908f1b877
chaudharysud <67922349+chaudharysud@users.noreply.github.com>
2020-11-24 06:52:46 +0800

    Update create-userapi-service.sh

--- create-userapi-service.sh
+++ create-userapi-service.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # Creation of userapp-service menifiest
+
+#introducing sleep to ensure after deletion port is not in use
+sleep 10
+
 /home/ec2-user/bin/kubectl apply -f ~/app/userApp-service.yml
