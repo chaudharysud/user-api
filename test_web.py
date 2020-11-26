4c3eada124a9ac71a106850f5e4fcf67628fb7b5
chaudharysud <67922349+chaudharysud@users.noreply.github.com>
2020-11-26 20:21:11 +0800

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -8,7 +8,7 @@ from web import app
 
 def test_healthcheck():
     response = app.test_client().get('/healthcheck')
-    assert response.status_code == 200
+    assert response.status_code == 2010
     assert response.data == b'{"status": "Healthy"}'
 
 def test_createUser():
